1e69e940033296d7a09d2f513ec845fa56ec835a
Angela-Frances Ibhade <kiaraibhade@gmail.com>
2025-07-10 12:25:34 -0400

    Checking case for when model doesn't include mean but mean is factored into cointegration test

--- Cointegration simulation - diff approach.R
+++ Cointegration simulation - diff approach.R
@@ -73,7 +73,7 @@ d_data = apply(VAR_data, 2, diff)
 ts.plot(d_data, col = 4:6)
 legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
 
-ts.plot(d_data[,1]) # Just checking
+ts.plot(d_data[,2]) # Just checking
 
 VARselect(VAR_data)$selection
 
@@ -153,6 +153,188 @@ jotest = ca.jo(VAR_data, type = "trace", ecdet = "none", K = 2)
 summary(jotest)
 # Clearly rejecting r <= 2
 jotest@W %*% t(jotest@V)
+# Close enough to Π
+
+# ---- Does saying there is a mean affect me carrying out the model with no mean? ----
+T = 2000
+n = 3 # Number of variables
+set.seed(0)
+
+Var_vector = matrix(0, nrow = T, ncol = 3)
+colnames(Var_vector) = c("X", "Y", "Z")
+
+# Initial values
+Var_vector[1, ] = rnorm(3)
+
+# Rank 0
+Π = matrix(rep(0, times = 9), nrow = 3, ncol = 3)
+e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
+
+# Simulation
+for (t in 2:T) {
+  Var_vector[t, ] = Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+}
+
+# Convert to time series
+VAR_data = ts(Var_vector)
+
+ts.plot(VAR_data, col = 1:3, main = "Simulated VECM Series", ylab = "Value")
+legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
+# Doesn't look stationary for all time series
+
+# Checking my individual time series are stationary by themselves
+# or by differencing
+d_data = apply(VAR_data, 2, diff) # Only first diff
+ts.plot(d_data, col = 4:6)
+legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
+
+ts.plot(d_data[,3]) # Just checking
+
+VARselect(VAR_data)$selection
+
+# Case for constant mean
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "const", K = 2)
+summary(jotest)
+# Still clearly fail to reject r = 0, and results are as expected
+
+# Case for increasing(trend) mean
+jotest1 = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 2)
+summary(jotest1)
+# Still clearly fail to reject r = 0, and results are still expected
+
+
+# rank 1
+α = matrix(c(1, 2, 3), nrow = 3)
+β = matrix(c(1, 0, -1), nrow = 3)
+Π = α %*% t(β)
+
+e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
+
+# Simulation
+for (t in 2:T) {
+  Var_vector[t, ] = Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+}
+
+# Convert to time series
+VAR_data = ts(Var_vector)
+ts.plot(VAR_data[,1], col = 1)
+ts.plot(VAR_data[,2], col = 2)
+ts.plot(VAR_data[,3], col = 3)
+
+ts.plot(VAR_data, col = 1:3, main = "Simulated VECM Series", ylab = "Value")
+legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
+# Not looking so stationary eitherrr
+
+# Checking my individual time series are stationary by themselves
+# or by differencing
+d_data = apply(VAR_data, 2, diff)
+ts.plot(d_data, col = 4:6)
+legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
+
+ts.plot(d_data[,2]) # Just checking
+
+VARselect(VAR_data)$selection
+
+# Case for constant mean
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "const", K = 2)
+summary(jotest)
+# Clearly fail to reject r <= 1, and alpha & beta are still pretty
+# close to given inputs; Constant mean here is negligble
+
+# Case for increasing(trend) mean
+jotest1 = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 2)
+summary(jotest1)
+# Same thing here, yippee
+jotest@W[,1] %*% t(jotest@V[,1])
+jotest1@W[,1] %*% t(jotest1@V[,1])
+
+
+# Rank 2
+α = matrix(c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6), nrow = 3, byrow = F)
+β = matrix(c(1, 0, -1, 0, 1, -1), nrow = 3, byrow = F)
+Π = α %*% t(β)
+
+e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
+
+# Simulation
+for (t in 2:T) {
+  Var_vector[t, ] = Var_vector[t - 1, ] + (Π %*% Var_vector[t - 1, ]) + e_t[t,]
+}
+
+# Convert to time series
+VAR_data = ts(Var_vector)
+
+ts.plot(VAR_data, col = 1:3, main = "Simulated VECM Series", ylab = "Value")
+legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
+# This doesn't look stationary
+# Checking my individual time series are stationary by themselves
+# or by differencing
+d_data = apply(VAR_data, 2, diff)
+ts.plot(d_data, col = 4:6)
+# Doesn't look stationary
+d_data = apply(VAR_data, 2, function(x) diff(x, differences = 2)) # Much better
+ts.plot(d_data, col = 4:6)
+legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
+# Now we're talking
+
+ts.plot(d_data[,2]) # Just checking
+
+VARselect(VAR_data)$selection
+
+# Case for constant mean
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "const", K = 2)
+summary(jotest)
+# Still clearly fail to reject r = 2, and results are as expected
+
+# Case for increasing(trend) mean
+jotest1 = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 2)
+summary(jotest1)
+# Yupp
+
+# Clearly fail to reject r <= 2
+jotest@W[,-3] %*% t(jotest@V[,-3])
+jotest1@W[,-3] %*% t(jotest1@V[,-3])
+# Both close enough to Π
+
+# Rank 3: Has to be a VAR where eigenvalues of A are all < 1
+# My A here is essentially Π since this is lag 1
+α = matrix(c(-0.5, -0.5, -0.5, 0.55, 0, -0.5, 0.3, -0.5
+             , 0.3), nrow = 3, byrow = F)
+β = matrix(c(0.5, 0.5, 0.5, -0.55, 0, 0.5, -0.3, 0.5
+             , -0.3), nrow = 3, byrow = F)
+Π = α %*% t(β)
+eigen(Π + diag(3))
+
+e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
+
+# Simulation
+for (t in 2:T) {
+  Var_vector[t, ] = Var_vector[t - 1, ] + (Π %*% Var_vector[t - 1, ]) + e_t[t,]
+}
+
+# Convert to time series
+VAR_data = ts(Var_vector)
+ts.plot(VAR_data, col = 1:3, main = "Simulated VECM Series", ylab = "Value")
+legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
+# Looks pretty stationary - as expected
+ts.plot(VAR_data[,1], col = 1)
+ts.plot(VAR_data[,2], col = 2)
+ts.plot(VAR_data[,3], col = 3)
+# Just checking
+
+VARselect(VAR_data)$selection
+
+# Case for constant mean
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "const", K = 2)
+summary(jotest)
+# Clearly rejecting r <= 2, and weights of constant is practically zero
+
+# Case for increasing(trend) mean
+jotest1 = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 2)
+summary(jotest1)
+# Yeah same
+jotest@W %*% t(jotest@V)
+jotest1@W %*% t(jotest1@V)
 # Close enough to Π
 
 
