c5a7bd9c4e4196141489f0fb2ac05cfd346910fd
Angela-Frances Ibhade <kiaraibhade@gmail.com>
2025-07-14 00:05:09 -0400

    Updated full portion on linear deterministic mean

--- Cointegration simulation - diff approach.R
+++ Cointegration simulation - diff approach.R
@@ -496,16 +496,22 @@ Var_vector[1, ] = rnorm(3)
 
 # Rank 0
 Π = matrix(rep(0, times = 9), nrow = 3, ncol = 3)
-e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
+Π_plus = matrix(0, n, n+1)
 
-mu1 = cumsum(abs(rnorm(T)))
-mu2 = cumsum(abs(rnorm(T)))
-mu3 = cumsum(abs(rnorm(T)))
-mu = cbind(mu1, mu2, mu3)
+mu0 = c(1, 2, 3)
+mu1 = c(0.025, 0.05, 0.0015)
+mu_t = matrix(NA, nrow = T, ncol = n)
+
+ν = -Π %*% (matrix(mu0, ncol = 1)) + (matrix(mu1, ncol = 1))
+
+e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
 
 # Simulation
 for (t in 2:T) {
-  Var_vector[t, ] = mu[t, ] + Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+  Var_lag = Var_vector[t - 1,]
+  x = c(Var_lag, t - 1)
+  Var_vector[t, ] = ν + Var_vector[t - 1, ] + 
+    (Π_plus %*% x) + e_t[t,]
 }
 
 # Convert to time series
@@ -519,19 +525,15 @@ legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
 # Checking my individual time series are stationary by themselves
 # or by differencing
 d_data = apply(VAR_data, 2, diff)
-ts.plot(d_data, col = 4:6) # Not doing it easily
+ts.plot(d_data, col = 4:6) # Sharp
 
-d_data = apply(VAR_data, 2, function(x) diff(x, differences = 2)) # Much better
-ts.plot(d_data, col = 4:6)
-legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
+VARselect(VAR_data, lag.max = 20)$selection # Saying lag of 1/2
 
-ts.plot(d_data[,3]) # Just checking
-
-VARselect(VAR_data)$selection # Saying lag of 5-6
-
-jotest = ca.jo(VAR_data, type = "trace", ecdet = "none", K = 5)
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 2)
 summary(jotest)
 # Clearly fail to reject r = 0
+jotest1 = ca.jo(VAR_data, type = 'trace', ecdet = 'none', K = 2)
+summary(jotest1) # Still failing to reject
 
 # rank 1
 α = matrix(c(1, 2, 3), nrow = 3)
@@ -539,14 +541,23 @@ summary(jotest)
 Π = α %*% t(β)
 
 e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
-mu1 = cumsum(abs(rnorm(T)))
-mu2 = cumsum(abs(rnorm(T)))
-mu3 = cumsum(abs(rnorm(T)))
-mu = cbind(mu1, mu2, mu3)
+mu0 = c(1, 2, 3)
+mu1 = c(0.025, 0.05, 0.0015)
+mu_t = matrix(NA, nrow = T, ncol = n)
+mu_t[1,] = mu0 + mu1
+
+η = -t(β)%*%mu1
+
+Π_plus = α %*% cbind(t(β), η)
+
+ν = -Π %*% (matrix(mu0, ncol = 1)) + (matrix(mu1, ncol = 1))
 
 # Simulation
 for (t in 2:T) {
-  Var_vector[t, ] = mu[t, ] + Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+  Var_lag = Var_vector[t - 1,]
+  x = c(Var_lag, t - 1)
+  Var_vector[t, ] = ν + Var_vector[t - 1, ] + 
+    (Π_plus %*% x) + e_t[t,]
 }
 
 # Convert to time series
@@ -561,20 +572,22 @@ legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
 d_data = apply(VAR_data, 2, diff)
 ts.plot(d_data, col = 4:6) # Not doing it easily
 
-d_data = apply(VAR_data, 2, function(x) diff(x, differences = 2)) # Much better
-ts.plot(d_data, col = 4:6)
-legend("topleft", legend = colnames(Var_vector), col = 4:6, lty = 1)
-
 ts.plot(d_data[,2]) # Just checking
 adf.test(d_data[,3])
 adf.test(VAR_data[,1])
 
-VARselect(VAR_data)$selection # Saying lag of 5-6
+VARselect(VAR_data, lag.max = 20)$selection # Saying lag of 14,8,7
 
-jotest = ca.jo(VAR_data, type = "trace", ecdet = "none", K = 2)
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 8)
 summary(jotest)
 # Clearly fail to reject r <= 1
 jotest@W[,1] %*% t(jotest@V[,1])
+
+# Me saying no trend/mean
+jotest1 = ca.jo(VAR_data, type = 'trace', ecdet = 'none', K = 2) # Lag 8 shows failing to reject 
+summary(jotest1) # Still failing to reject
+jotest1@W[,1] %*% t(jotest1@V[,1]) # Def not the same as Π+
+
 # Similar results as no mean
 
 # Rank 2
@@ -583,15 +596,23 @@ jotest@W[,1] %*% t(jotest@V[,1])
 Π = α %*% t(β)
 
 e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
-# Had to scale this down
-mu1 = 0.1 * cumsum(abs(rnorm(T)))
-mu2 = 0.1 * cumsum(abs(rnorm(T)))
-mu3 = 0.1 * cumsum(abs(rnorm(T)))
-mu = cbind(mu1, mu2, mu3)
+mu0 = c(1, 2, 3)
+mu1 = c(0.025, 0.05, 0.0015)
+mu_t = matrix(NA, nrow = T, ncol = n)
+mu_t[1,] = mu0 + mu1
+
+η = -t(β)%*%mu1
+
+Π_plus = α %*% cbind(t(β), η)
+
+ν = -Π %*% (matrix(mu0, ncol = 1)) + (matrix(mu1, ncol = 1))
 
 # Simulation
 for (t in 2:T) {
-  Var_vector[t, ] = mu[t, ] + Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+  Var_lag = Var_vector[t - 1,]
+  x = c(Var_lag, t - 1)
+  Var_vector[t, ] = ν + Var_vector[t - 1, ] + 
+    (Π_plus %*% x) + e_t[t,]
 }
 
 # Convert to time series
@@ -615,16 +636,14 @@ ts.plot(d_data[,2]) # Just checking
 adf.test(d_data[,3])
 adf.test(VAR_data[,2])
 
-VARselect(VAR_data)$selection # 10,9,6,10
+VARselect(VAR_data)$selection # 10,8,6,10
 
-jotest = ca.jo(VAR_data, type = "trace", ecdet = "none", K = 2)
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 9)
 summary(jotest)
 # Clearly fail to reject r <= 2
 jotest@W[,-3] %*% t(jotest@V[,-3])
-# Close enough to Π
+# Close enough to Π+: nice!
 
-################### 
-# Come back to this to edit
 # Rank 3: Has to be a VAR where eigenvalues of A are all < 1
 # My A here is essentially Π since this is lag 1
 α = matrix(c(-0.5, -0.5, -0.5, 0.55, 0, -0.5, 0.3, -0.5
@@ -632,13 +651,25 @@ jotest@W[,-3] %*% t(jotest@V[,-3])
 β = matrix(c(0.5, 0.5, 0.5, -0.55, 0, 0.5, -0.3, 0.5
              , -0.3), nrow = 3, byrow = F)
 Π = α %*% t(β)
-eigen(Π + diag(3))
 
 e_t = matrix(rnorm(T * n), nrow = T, ncol = n)
-mu = as.matrix(rep(2, times = T*n), nrow = T, ncol = n)
+mu0 = c(1, 2, 3)
+mu1 = c(0.025, 0.05, 0.0015)
+mu_t = matrix(NA, nrow = T, ncol = n)
+mu_t[1,] = mu0 + mu1
+
+η = -t(β)%*%mu1
+
+Π_plus = α %*% cbind(t(β), η)
+
+ν = -Π %*% (matrix(mu0, ncol = 1)) + (matrix(mu1, ncol = 1))
+
 # Simulation
 for (t in 2:T) {
-  Var_vector[t, ] = mu[t, ] + Var_vector[t - 1, ] + Π %*% Var_vector[t - 1, ] + e_t[t,]
+  Var_lag = Var_vector[t - 1,]
+  x = c(Var_lag, t - 1)
+  Var_vector[t, ] = ν + Var_vector[t - 1, ] + 
+    (Π_plus %*% x) + e_t[t,]
 }
 
 # Convert to time series
@@ -646,12 +677,14 @@ VAR_data = ts(Var_vector)
 
 ts.plot(VAR_data, col = 1:3, main = "Simulated VECM Series", ylab = "Value")
 legend("topleft", legend = colnames(Var_vector), col = 1:3, lty = 1)
-# Looks stationary
-
+# Not stationary but expected due to trend
+d_data = apply(VAR_data, 2, diff)
+ts.plot(d_data, col = 4:6) 
+# Nice
 VARselect(VAR_data)$selection
 
-jotest = ca.jo(VAR_data, type = "trace", ecdet = "none", K = 2)
+jotest = ca.jo(VAR_data, type = "trace", ecdet = "trend", K = 6)
 summary(jotest)
 # Clearly rejecting r <= 2
 jotest@W %*% t(jotest@V)
-# Close enough to Π
+# Close enough to Π_plus
