e3d7112fc6511ca9cb150a71812746a55808c6bb
Angela-Frances Ibhade <kiaraibhade@gmail.com>
2025-07-27 23:02:22 -0400

    Example to depict stationarity of AR models

---
+++ Stationarity of AR models - Examples.R
@@ -0,0 +1,56 @@
+rm(list = ls())
+T = 500
+
+set.seed(0)
+y =numeric(T)
+e = numeric(T)
+
+y[1] = rnorm(1, 0, 1)
+e[1] = rnorm(1, 0, 1)
+
+
+for (t in 2:T) {
+  e[t] = rnorm(1, 0, 1)
+  y[t] = (0.2 * y[t - 1]) + e[t]
+}
+
+ts.plot(y)
+
+# AR(2) - nonstat
+T = 500
+
+set.seed(0)
+y =numeric(T)
+e = numeric(T)
+
+y[1] = rnorm(1, 0, 1)
+y[2] = rnorm(1, 0, 1)
+e[1] = rnorm(1, 0, 1)
+e[2] = rnorm(1, 0, 1)
+
+for (t in 3:T) {
+  e[t] = rnorm(1, 0, 1)
+  y[t] = (0.2 * y[t - 1]) + (0.8 * y[t - 2]) + e[t]
+}
+
+ts.plot(y)
+
+# Stat AR(2)
+T = 500
+
+set.seed(0)
+y =numeric(T)
+e = numeric(T)
+
+y[1] = rnorm(1, 0, 1)
+y[2] = rnorm(1, 0, 1)
+e[1] = rnorm(1, 0, 1)
+e[2] = rnorm(1, 0, 1)
+
+for (t in 3:T) {
+  e[t] = rnorm(1, 0, 1)
+  y[t] = (-0.2 * y[t - 1]) + (0.3 * y[t - 2]) + e[t]
+}
+
+ts.plot(y)
+
